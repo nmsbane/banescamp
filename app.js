d2ec6f393e119e52988adc51d03de3e6a5e2311d
Madhusudan <madhusudhan638@gmail.com>
2016-04-10 07:32:03 +0000

    implemented login functionality with out styling, now the register user can login into our app

--- app.js
+++ app.js
@@ -148,6 +148,22 @@ app.post('/register', function(req, res) {
    });
 });
 
+// show the login form when user visits /login
+app.get('/login', function(req, res) {
+   res.render('login') ;
+});
+
+// handle the post request from the login page
+app.post('/login', passport.authenticate("local", 
+    {
+        successRedirect: '/campgrounds',
+        failureRedirect: '/login'
+    }
+), function(req, res) {
+    
+});
+
+
 app.listen(process.env.PORT, process.env.IP, function() {
     console.log("The camp server has started");
 });

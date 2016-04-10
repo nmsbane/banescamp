64afc8435a9d55767b629b5e755edabe47a32bd8
Madhusudan <madhusudhan638@gmail.com>
2016-04-10 18:45:46 +0000

    created a route to delete a comment. This route also follows the rest principles

--- routes/comments.js
+++ routes/comments.js
@@ -55,8 +55,19 @@ router.put('/:comment_id', function(req, res) {
         } else {
             res.redirect("/campgrounds/" + req.params.id);
         }
-    })
-})
+    });
+});
+
+// DELETE THE COMMENT
+router.delete('/:comment_id', function(req, res) {
+   Comment.findByIdAndRemove(req.params.comment_id, function(err) {
+       if(err) {
+           res.redirect("back");
+       } else {
+           res.redirect("/campgrounds/" + req.params.id);
+       }
+   });
+});
 
 // create a middleware which will check if the user is logged in or not
 function isLoggedIn(req, res, next) {

cca895cc9b6fcdbb1ceda5af269e8f00819643fd
Madhusudan <madhusudhan638@gmail.com>
2016-04-10 11:11:36 +0000

    now the user cannot create a new campground unless he is logged in, used the isLogggedIn middleware for the creation of new campground. Also changed the model of campground to show the author, which by default points to the currently logged in user

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -15,17 +15,18 @@ router.get('/', function(req, res) {
 });
 
 
-router.post('/', function(req, res) {
+router.post('/', isLoggedIn, function(req, res) {
     // get the form data
     var name = req.body.name;
     var image = req.body.image;
     var description = req.body.description;
     
-    
+    var author = {id: req.user._id, username: req.user.username }
     var campground = {
         name: name,
         image: image,
-        description: description
+        description: description,
+        author: author
     };
     // create a new campground in the database
     Campground.create(campground, function(err, camp) {
@@ -37,7 +38,7 @@ router.post('/', function(req, res) {
     })
 });
 
-router.get('/new', function(req, res) {
+router.get('/new',isLoggedIn,  function(req, res) {
    res.render('campgrounds/new') ;
 });
 
@@ -52,5 +53,14 @@ router.get('/:id', function(req, res) {
     })
     
 });
+
+// create a middleware which will check if the user is logged in or not
+function isLoggedIn(req, res, next) {
+    if(req.isAuthenticated()) {
+        return next();   
+    } 
+    res.redirect('/login');
+}
+
 
 module.exports = router;

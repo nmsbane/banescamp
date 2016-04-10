6828cdbd3317476492525f0fb25294285c4d2acf
Madhusudan <madhusudhan638@gmail.com>
2016-04-10 04:14:35 +0000

    created custom stylesheets, to make image take entire width, used express.static middleware to locate public directory, from which all the custom css and js files will be served

--- app.js
+++ app.js
@@ -13,6 +13,9 @@ mongoose.connect("mongodb://localhost/banes_camp");
 
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.use(express.static(__dirname + '/public'));
+
+
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {

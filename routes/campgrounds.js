79d25aa711d3630a6caadaf5fe681637790c2767
Madhusudan <madhusudhan638@gmail.com>
2016-04-10 15:26:55 +0000

    created a link to delete a campground, and wrote backend code to find the camp and delete the camp from the db. The special about this is the fact that the routes are very restful

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -76,6 +76,19 @@ router.put('/:id', function(req, res) {
         }
    }); 
 });
+
+// DELETE A CAMPGROUND
+router.delete('/:id', function(req, res) {
+    Campground.findByIdAndRemove(req.params.id, function(err) {
+        if(err) {
+            res.redirect('/campgrounds');
+        } else {
+            res.redirect('/campgrounds');
+        }
+    });  
+});
+
+
 // create a middleware which will check if the user is logged in or not
 function isLoggedIn(req, res, next) {
     if(req.isAuthenticated()) {
